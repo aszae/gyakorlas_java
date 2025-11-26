416160f67f1a03355aa02d42719cd31eaa06d18e
Evila <aszalosevi@gmail.com>
2025-11-26 21:48:37 +0100

    Added MatrixDeterminant.java to src/gyak2 and compiled it to bytecode (MatrixDeterminant.class)

--- src/gyak2/MatrixDeterminant.java
+++ src/gyak2/MatrixDeterminant.java
@@ -18,6 +18,6 @@ public class MatrixDeterminant {
                 matrix[2][0] * matrix[1][1] * matrix[0][2] -
                 matrix[1][0] * matrix[0][1] * matrix[2][2] -
                 matrix[2][1] * matrix[1][2] * matrix[0][0];
-        System.out.printf("Determinant = %.2f", determinant);
+        System.out.printf("Determinant = %.2f\n", determinant);
     }
 }
